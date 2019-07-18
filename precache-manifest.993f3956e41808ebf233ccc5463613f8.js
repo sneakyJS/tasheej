15e1da438e490b031543bae188521549a975017e
Ahd <dafuq.ahd@gmail.com>
2019-07-18 09:10:23 +0300

    Updates

---
+++ precache-manifest.993f3956e41808ebf233ccc5463613f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7712b1f68891cfdaff9c010801ab57f6",
+    "url": "/tasheej/index.html"
+  },
+  {
+    "revision": "dca1f3c63225966dd97c",
+    "url": "/tasheej/static/css/main.1fa81ff4.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/tasheej/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "dca1f3c63225966dd97c",
+    "url": "/tasheej/static/js/main.7f54a7c6.chunk.js"
+  },
+  {
+    "revision": "2b5657495ba12a3a7216",
+    "url": "/tasheej/static/js/runtime~main.72cb45b4.js"
+  },
+  {
+    "revision": "cf44dea94e900d7305635d34bdf3a102",
+    "url": "/tasheej/static/media/main_900.cf44dea9.jpg"
+  }
+]);
